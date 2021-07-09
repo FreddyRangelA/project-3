8ce480fd492fd22b6d5ce4858f1e849d6b3419e5
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-07-08 19:26:37 -0500

    js update

--- static/js/app.js
+++ static/js/app.js
@@ -11,4 +11,33 @@ function runEnter() {
     d3.event.preventDefault();
 
     var inputstr = d3.select("#input").property("value");
+    d3.json(`/predict/${inputstr}`).then(d => {
+      d3.select("#result").text(`the model predicts your ${d}`);
+      predictedResult = d;
+  });
 }
+
+function correct() {
+  d3.event.preventDefault();
+  var ans=["negative", "positive"] 
+  outcome = ans.indexOf(predictedResult)
+  updateData(outcome)
+}
+
+function wrong() {
+  d3.event.preventDefault();
+  var ans=[ "positive","negative"] 
+  outcome = ans.indexOf(predictedResult)
+  updateData(outcome)
+}
+
+function updateData(outcome) {
+
+  var inputstr = d3.select("#input").property("value");
+
+  d3.json(`/predict/${inputstr}`).then(d=>console.log(d));
+
+}
+
+d3.select('#yes').on("click",correct)
+d3.select('#no').on("click",wrong)
