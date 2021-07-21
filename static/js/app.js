1ce20bdca8ca1877d96405c6f028474d9deb6a29
freddy31_ag@hotmail.com <freddy31_ag@hotmail.com>
2021-07-20 19:38:10 -0500

    wk

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,7 @@ var form = d3.select("#form");
 // Create event handlers for clicking the button or pressing the enter key
 // button.on("click", runEnter);
 form.on("submit", runEnter);
+
 var predictedResult = '';
 // Create the function to run for both events
 function runEnter() {
@@ -10,9 +11,24 @@ function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    var inputstr = d3.select("#input").property("value");
+    var inputstr = d3.select("#comments").property("value");
     d3.json(`/predict/${inputstr}`).then(d => {
-      d3.select("#result").text(`the model predicts your ${d}`);
+      console.log(d);
+      if (d.outcome == 0) {
+        d==("Neutral");
+        console.log("neutral");
+
+      }
+      else if (d==1){
+        d=="Negative ";
+        
+      }else{
+        d=="Positive";
+        
+      }
+
+      d3.select("#result").text(`the model predicts that your comment is: ${d} of Trump`);
+      console.log("cliecked")
       predictedResult = d;
   });
 }
